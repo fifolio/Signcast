86f1ab54a6ca20d0a79a414ae3643aa41ad2e61b
Firas <FirasDabbabi@gmail.com>
2024-12-05 17:00:51 +0100

    Update Navbar.tsx

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
         </div>
         <div className="sm:text-sm text-normal">
           <button className="bg-blue-500 border-blue-600 hover:bg-blue-600 border-[1px] font-semibold uppercase text-white px-6 py-2 rounded-md shadow-sm space-x-2 flex items-center">
-            <div className="flex">Upload<span className="sm:block hidden mx-1">.CSV file</span></div>
+            <div className="flex">Upload .CSV<span className="sm:block hidden mx-1">file</span></div>
             <div className="text-lg"><MdOutlineUploadFile /></div>
           </button>
         </div>
